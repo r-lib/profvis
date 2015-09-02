17093aa86d88f4bbd62a3ca854621674a98963c1
Winston Chang <winston@stdout.org>
2015-09-02 15:59:19 -0500

    Add htmlwidgets scaffolding

---
+++ R/profvis.R
@@ -0,0 +1,38 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+profvis <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'profvis',
+    x,
+    width = width,
+    height = height,
+    package = 'profvis'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+profvisOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'profvis', width, height, package = 'profvis')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderProfvis <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, profvisOutput, env, quoted = TRUE)
+}
