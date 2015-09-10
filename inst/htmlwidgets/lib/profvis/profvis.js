ee4c36e81aa60fc9c3f9863a121544cbd2fb96f9
Winston Chang <winston@stdout.org>
2015-09-10 11:44:22 -0500

    Add skeleton profvis JS library

---
+++ inst/htmlwidgets/lib/profvis/profvis.js
@@ -0,0 +1,21 @@
+/*jshint
+  undef:true,
+  browser:true,
+  devel: true,
+  jquery:true,
+  strict:false,
+  curly:false,
+  indent:2
+*/
+/*global profvis */
+
+profvis = (function() {
+  var profvis = {};
+
+  profvis.linedata = function(filename) {
+
+  };
+
+  return profvis;
+
+})();
