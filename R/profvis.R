86b75eb8730c4ac3f12f7058e74ae409556bf527
Winston Chang <winston@stdout.org>
2015-10-27 15:41:13 -0500

    Allow profvis() to take a Rprof file as input

--- R/profvis.R
+++ R/profvis.R
@@ -1,11 +1,21 @@
-#' Run an R expression and record profiling information
+#' Profile an R expression and visualize profiling data
 #'
-#' @param expr Code to profile.
+#' This function will run an R expression with profiling, and then return an
+#' htmlwidget for interactively exploring the profiling data.
+#'
+#' An alternate way to use \code{profvis} is to separately capture the profiling
+#' data to a file using \code{\link{Rprof}()}, and then pass the path to the
+#' corresponding data file as the \code{prof_input} argument to
+#' \code{profvis()}.
+#'
+#' @param expr Code to profile. Not compatible with \code{prof_input}.
 #' @param interval Interval for profiling samples, in seconds. Values less than
 #'   0.005 (5 ms) will probably not result in accurate timings
-#' @param prof_file Name of an Rprof output file in which to save profiling
+#' @param prof_output Name of an Rprof output file in which to save profiling
 #'   data. If \code{NULL} (the default), a temporary file will be used and
 #'   automatically removed when the function exits.
+#' @param prof_input The path to an \code{\link{Rprof}} data file.  Not
+#'   compatible with \code{expr} or \code{prof_output}.
 #' @param width Width of the htmlwidget.
 #' @param height Height of the htmlwidget
 #'
@@ -14,44 +24,60 @@
 #'
 #' @import htmlwidgets
 #' @export
-profvis <- function(expr, interval = 0.01, prof_file = NULL, width = NULL, height = NULL) {
-  remove_on_exit <- FALSE
-  if (is.null(prof_file)) {
-    prof_file <- tempfile(fileext = ".prof")
-    remove_on_exit <- TRUE
-  }
+profvis <- function(expr = NULL, interval = 0.01, prof_output = NULL,
+                    prof_input = NULL, width = NULL, height = NULL)
+{
+  expr_q <- substitute(expr)
 
+  if (is.null(prof_input) && is.null(expr_q)) {
+    stop("profvis must be called with `expr` or `prof_input` ")
+  }
+  if (!is.null(prof_input) && (!is.null(expr_q) && !is.null(prof_output))) {
+    stop("The `prof_input` argument cannot be used with `expr` or `prof_output`.")
+  }
   if (interval < 0.005) {
     message("Intervals smaller than ~5ms will probably not result in accurate timings.")
   }
 
-  # Keep original expression source code
-  expr_source <- attr(substitute(expr), "wholeSrcref", exact = TRUE)
-  expr_source <- attr(expr_source, "srcfile", exact = TRUE)$lines
-  # Usually, $lines is a single string, but sometimes it can be split up into a
-  # vector. Make sure it's a single string.
-  expr_source <- paste(expr_source, collapse = "\n")
+  if (!is.null(expr_q)) {
+    # Keep original expression source code
+    expr_source <- attr(expr_q, "wholeSrcref", exact = TRUE)
+    expr_source <- attr(expr_source, "srcfile", exact = TRUE)$lines
+    # Usually, $lines is a single string, but sometimes it can be split up into a
+    # vector. Make sure it's a single string.
+    expr_source <- paste(expr_source, collapse = "\n")
 
-
-  gc()
-  Rprof(prof_file, interval = interval, line.profiling = TRUE,
-        gc.profiling = TRUE)
-  on.exit(Rprof(NULL), add = TRUE)
-  if (remove_on_exit)
-    on.exit(unlink(prof_file), add = TRUE)
-
-  tryCatch(
-    force(expr),
-    error = function(e) {
-      message("prof: code exited with error:\n", e$message, "\n")
-    },
-    interrupt = function(e) {
-      message("prof: interrupt received.")
+    remove_on_exit <- FALSE
+    if (is.null(prof_output)) {
+      prof_output <- tempfile(fileext = ".prof")
+      remove_on_exit <- TRUE
     }
-  )
-  Rprof(NULL)
 
-  message <- parse_rprof(prof_file, expr_source)
+    gc()
+    Rprof(prof_output, interval = interval, line.profiling = TRUE,
+          gc.profiling = TRUE)
+    on.exit(Rprof(NULL), add = TRUE)
+    if (remove_on_exit)
+      on.exit(unlink(prof_output), add = TRUE)
+
+    tryCatch(
+      force(expr),
+      error = function(e) {
+        message("profvis: code exited with error:\n", e$message, "\n")
+      },
+      interrupt = function(e) {
+        message("profvis: interrupt received.")
+      }
+    )
+    Rprof(NULL)
+
+  } else {
+    # If we got here, we were provided a prof_input file instead of expr
+    expr_source <- character(0)
+    prof_output <- prof_input
+  }
+
+  message <- parse_rprof(prof_output, expr_source)
 
   # Add sequences to collapse
   message$collapseItems <- collapseItems()
