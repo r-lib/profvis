f9ff64a9656e75b42e253358756b64e744fe5a3f
Javier Luraschi <javierluraschi@hotmail.com>
2016-03-18 15:17:41 -0700

    implement tabs, host treemap and basic styling

--- inst/htmlwidgets/lib/profvis/profvis.js
+++ inst/htmlwidgets/lib/profvis/profvis.js
@@ -14,26 +14,24 @@ profvis = (function() {
 
   profvis.render = function(el, message) {
 
-    function generateStatusBarButton(caption) {
+    function generateStatusBarButton(id, caption, active) {
       var spacerImage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAAUCAYAAACnOeyiAAAAXklEQVR42mNgAIL///8zMYSGhjIDGYIMIiIMvECGMwMDN4M4kFEDUqIIZKwDMdSBjAsghj6Q8QPEMAAy/lOBoQekv4AYKkDGfgZeXl4RICOLQUtLiw3IUAJJMQIZ7AC2tU2tXJxOYgAAAABJRU5ErkJggg==';
 
       var buttonHtml =
-        '<div class="info-block result-block active"><span class="info-label">' + caption + '</span></div>' +
+        '<div id="' + id  + '" class="info-block ' +
+        (active ? 'result-block-active' : 'result-block') +
+        '"><span class="info-label">' + caption + '</span></div>' +
         '<div class="separator-block"><img class="separator-image" src="' + spacerImage + '"></div>';
 
       return buttonHtml;
     }
 
-    function generateStatusBar(el) {
+    function generateStatusBar(el, onToogle) {
       var $el = $(el);
 
       el.innerHTML =
-        generateStatusBarButton('Flame graph') +
-        '<div class="spacing-block"></div>' +
-        '<div class="info-block"><span class="info-label">Total time:</span> ' +
-          vis.totalTime + 'ms</div>' +
-        '<div class="info-block"><span class="info-label">Sample interval:</span> ' +
-          vis.interval + 'ms</div>' +
+        generateStatusBarButton('flameGraphButton', 'Flame graph', true) +
+        generateStatusBarButton('treemapButton', 'Treemap', false) +
         '<span role="button" class="options-button">Options &#x25BE;</span>';
 
       $el.find("span.options-button").on("click", function(e) {
@@ -42,6 +40,36 @@ profvis = (function() {
 
         vis.optionsPanel.toggleVisibility();
       });
+
+      var setStatusBarButtons = function(e) {
+        $(".info-block").removeClass("result-block-active");
+        $(".info-block").addClass("result-block");
+        e.addClass("result-block-active");
+      }
+
+      $el.find("#flameGraphButton").on("click", function() {
+        setStatusBarButtons($(this));
+        onToogle("flamegraph");
+      });
+
+      $el.find("#treemapButton").on("click", function() {
+        setStatusBarButtons($(this));
+        onToogle("treemap");
+      });
+
+      return {
+        el: el
+      };
+    }
+
+    function generateFooter(el, onToogle) {
+      var $el = $(el);
+
+      el.innerHTML =
+        '<div class="info-block"><span class="info-label">Sample interval:</span> ' +
+          vis.interval + 'ms</div>' +
+        '<div class="info-block-right"><span class="info-label">Total time:</span> ' +
+          vis.totalTime + 'ms</div>';
 
       return {
         el: el
@@ -1198,6 +1226,98 @@ profvis = (function() {
       };
     }
 
+    function generateTreemap(el) {
+      var $el = $(el);
+
+      var tree = {
+          name: "tree",
+          children: [
+            {
+              name: "myf()",
+              size: 1.0,
+              level: 0,
+              children: [
+                { name: "pause1()", size: 0.5, level: 1 },
+                {
+                  name: "pause2()",
+                  size: 0.5,
+                  level: 1,
+                  children: [
+                    {
+                      name: "sys.time()",
+                      size: 0.5,
+                      level: 2,
+                      children: [
+                        { name: "a()", size: 0.1, level: 3 },
+                        { name: "b()", size: 0.1, level: 3 },
+                        { name: "c()", size: 0.1, level: 3 },
+                        { name: "d()", size: 0.1, level: 3 },
+                        { name: "e()", size: 0.1, level: 3 }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            { name: "myf2()", size: 1.0, level: 0 },
+          ]
+      };
+
+      var dims = {
+        margin: { top: 25, right: 0, left: 0, bottom: 25 }
+      };
+
+      var renderTreemap = function () {
+        var innerWidth = el.clientWidth; // - dims.margin.left - dims.margin.right;
+        var innerHeight = el.clientHeight; // - dims.margin.top - dims.margin.bottom;
+
+        var colorScale = d3.scale.linear()
+          .domain([0.0, 1.0])
+          .range(["rgb(241, 243, 246)", "rgb(233, 238, 243)"]);
+
+        var width = innerWidth,
+            height = innerHeight,
+            color = colorScale,
+            div = d3.select(el).append("div")
+               .style("position", "relative");
+
+        var treemap = d3.layout.treemap()
+            .size([width, height])
+            .sticky(true)
+            .value(function(d) { return d.size; });
+
+        function position() {
+          this.style("left", function(d) { return d.x + "px"; })
+            .style("top", function(d) { return d.y + "px"; })
+            .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+            .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+        }
+
+        var node = div.datum(tree).selectAll(".node")
+            .data(treemap.nodes)
+          .enter().append("div")
+            .attr("class", "node")
+            .call(position)
+            .style("background-color", function(d) {
+              if (d.name == 'tree') return "#FFF";
+              if (d.children) return color(d.size);
+              return color(d.size);
+            })
+            .append('div')
+            .style("font-size", function(d) {
+                return Math.min(12, 0.18*Math.sqrt(d.area))+'px';
+            })
+            .text(function(d) { return d.children ? null : d.name; });
+      };
+
+      renderTreemap();
+
+      return {
+        el: el,
+        onResize: renderTreemap
+      }
+    }
+
     function enableScroll() {
       vis.codeTable.enableScroll();
       vis.flameGraph.enableZoom();
@@ -1216,6 +1336,7 @@ profvis = (function() {
       var $el = $(vis.el);
       var $panel1 = $el.children(".profvis-panel1");
       var $panel2 = $el.children(".profvis-panel2");
+      var $panelTreemap = $el.children(".profvis-treemap");
       var $splitBar = $el.children(".profvis-splitbar");
 
       var splitBarGap;
@@ -1291,6 +1412,7 @@ profvis = (function() {
         debounce(function() {
           resizePanels(lastSplitProportion);
           vis.flameGraph.onResize();
+          vis.treemap.onResize();
         }, 250)
       );
 
@@ -1445,6 +1567,10 @@ profvis = (function() {
 
     // Render the objects ---------------------------------------------
 
+    var statusBarEl = document.createElement("div");
+    statusBarEl.className = "profvis-status-bar";
+    vis.el.appendChild(statusBarEl);
+
     // Container panels - top/bottom or left/right
     var panel1 = document.createElement("div");
     panel1.className = "profvis-panel1 profvis-panel1-" + splitClass;
@@ -1458,11 +1584,11 @@ profvis = (function() {
     splitBarEl.className = "profvis-splitbar profvis-splitbar-" + splitClass;
     vis.el.appendChild(splitBarEl);
 
-    // Items in the panels
-    var statusBarEl = document.createElement("div");
-    statusBarEl.className = "profvis-status-bar";
-    panel1.appendChild(statusBarEl);
+    var footerEl = document.createElement("div");
+    footerEl.className = "profvis-footer";
+    vis.el.appendChild(footerEl);
 
+    // Items in the panels
     var codeTableEl = document.createElement("div");
     codeTableEl.className = "profvis-code";
     panel1.appendChild(codeTableEl);
@@ -1479,17 +1605,47 @@ profvis = (function() {
     infoBoxEl.className = "profvis-infobox";
     panel2.appendChild(infoBoxEl);
 
+    var treemapEl = document.createElement("div");
+    treemapEl.className = "profvis-treemap";
+    treemapEl.style.display = "none";
+    vis.el.appendChild(treemapEl);
 
     // Efficient to properly size panels before the code + flamegraph are
     // rendered, so that we don't have to re-render.
     initResizing(message.split);
 
+    var hideViews = function() {
+      splitBarEl.style.display = "none";
+      treemapEl.style.display = "none";
+      panel1.style.display = "none";
+      panel2.style.display = "none";
+    }
+
+    var toggleViews = function(view) {
+      switch (view) {
+        case "flamegraph":
+          hideViews();
+          splitBarEl.style.display = "block";
+          panel1.style.display = "block";
+          panel2.style.display = "block";
+          vis.flameGraph.onResize();
+          break;
+        case "treemap":
+          hideViews();
+          treemapEl.style.display = "block";
+          vis.treemap.onResize();
+          break;
+      }
+    }
+
     // Create the UI components
-    vis.statusBar = generateStatusBar(statusBarEl);
+    vis.statusBar = generateStatusBar(statusBarEl, toggleViews);
+    vis.footer = generateFooter(footerEl);
     vis.optionsPanel = generateOptionsPanel(optionsPanelEl);
     vis.codeTable = generateCodeTable(codeTableEl);
     vis.flameGraph = generateFlameGraph(flameGraphEl);
     vis.infoBox = initInfoBox(infoBoxEl);
+    vis.treemap = generateTreemap(treemapEl);
 
     // If any depth collapsing occured, enable the "hide internal" checkbox.
     if (prof.some(function(d) { return d.depth !== d.depthCollapsed; })) {
