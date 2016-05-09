3d7f9d9489fb855cdb1526dd55064ce90510a111
Winston Chang <winston@stdout.org>
2016-05-09 11:09:55 -0500

    Refinements to parsing test

--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -3,20 +3,21 @@ context("parse")
 test_that("parsing prof files", {
   # The three lines in this file have three different kinds of call stacks.
   # They start with:
-  # "test" 1#1
+  # "test space" 1#1
   # 1#2 "test" 1#1
   # "<GC>" 1#2 "test" 1#1
+  # "<GC>" "test" 1#1
   p <- parse_rprof("test-parse.prof", expr_source = "line 1\nline 2")
 
-  expect_identical(p$prof$time, c(1L, 2L, 2L, 3L, 3L, 3L))
-  expect_identical(p$prof$depth, c(1L, 2L, 1L, 3L, 2L, 1L))
-  expect_identical(p$prof$label, c("test space", "line 2", "test", "<GC>", "line 2", "test"))
-  expect_identical(p$prof$filenum, c(1L, 1L, 1L, NA, 1L, 1L))
-  expect_identical(p$prof$linenum, c(1L, 2L, 1L, NA, 2L, 1L))
+  expect_identical(p$prof$time, c(1L, 2L, 2L, 3L, 3L, 3L, 4L, 4L))
+  expect_identical(p$prof$depth, c(1L, 2L, 1L, 3L, 2L, 1L, 2L, 1L))
+  expect_identical(p$prof$label, c("test space", "line 2", "test", "<GC>", "line 2", "test", "<GC>", "test"))
+  expect_identical(p$prof$filenum, c(1L, 1L, 1L, NA, 1L, 1L, NA, 1L))
+  expect_identical(p$prof$linenum, c(1L, 2L, 1L, NA, 2L, 1L, NA, 1L))
   # Memory sizes in the test-parse.prof file were chosen to create these values
-  expect_identical(p$prof$memalloc, c(136, 152, 152, 168, 168, 168))
-  expect_identical(p$prof$meminc, c(0, 16, 0, 16, 0, 0))
-  expect_identical(p$prof$filename, c("<expr>", "<expr>", "<expr>", NA, "<expr>", "<expr>"))
+  expect_identical(p$prof$memalloc, c(136, 152, 152, 168, 168, 168, 152, 152))
+  expect_identical(p$prof$meminc, c(0, 16, 0, 16, 0, 0, -16, 0))
+  expect_identical(p$prof$filename, c("<expr>", "<expr>", "<expr>", NA, "<expr>", "<expr>", NA, "<expr>"))
 
   expect_identical(p$interval, 10)
   expect_identical(p$files,
