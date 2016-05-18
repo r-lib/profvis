7177fe48c7b8b47e7618ed931e90a11d1c462cce
Javier Luraschi <javierluraschi@hotmail.com>
2016-05-18 10:15:15 -0700

    add test to prevent to ensure package builds with older versions of r

---
+++ tests/testthat/test-integration.R
@@ -0,0 +1,12 @@
+context("integration")
+
+test_that("description file has compatible fields", {
+  # Other projects, like RStudio, depend on multiple version of R being supported.
+  # Therefore, worth validating the DESCRIPTION field is backwards compatiable for
+  # them to build against older versions of R.
+
+  descriptionFile <- yaml::yaml.load_file(file.path(getwd(), "..", "..", "DESCRIPTION"))
+
+  expect_false(identical(descriptionFile[["Author"]], NULL))
+  expect_false(identical(descriptionFile[["Maintainer"]], NULL))
+})
