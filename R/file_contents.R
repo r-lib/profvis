c72ce8f3b9c721a205b9e6d2ba0e62d6b8de1916
Winston Chang <winston@stdout.org>
2016-10-31 14:59:17 -0500

    More reliable way of getting file contents.Closes #68

--- R/file_contents.R
+++ R/file_contents.R
@@ -1,96 +1,43 @@
 get_file_contents <- function(filenames, expr_source) {
+  filenames <- filenames[filenames != ""]
   names(filenames) <- filenames
 
-  srcref_cache <- new.env(parent = emptyenv())
+  srcfile_cache <- build_srcfile_cache()
+  srcfile_cache[["<expr>"]] <- expr_source
 
   file_contents <- lapply(filenames, function(filename) {
-    if (filename == "<expr>") {
-      return(expr_source)
-    }
-
-    filehandle <- tryCatch(
-      file(filename, 'rb'),
-      error = function(e) NULL,
-      warning = function(e) NULL
-    )
-    # If we can't read file, attempt to extract using source refs
-    if (is.null(filehandle)) {
-      return(extract_source_from_srcref(filename, srcref_cache))
-    }
-    on.exit( close(filehandle) )
-
-    readChar(filename, file.info(filename)$size, useBytes = TRUE)
+    srcfile_cache[[filename]]
   })
 
   drop_nulls(file_contents)
 }
 
 
-# Given a filename, try to get the source code from source refs. This looks
-# inside a package namespace to try to get the source code.
-# `filename` will be something like:
-#   "/tmp/Rtmp6W0MLC/R.INSTALL1a531f3beb59/ggplot2/R/aes.r"
-extract_source_from_srcref <- function(filename, srcref_cache) {
-  # Filename must have format "xxx/yyy/R/zzz.r", where the "xxx" part can be
-  # anything.
-  if (!grepl(".*/([^/]+)/R/[^/]+", filename)) {
-    return(NULL)
-  }
+build_srcfile_cache <- function(pkgs = loadedNamespaces()) {
+  srcfile_cache <- new.env(parent = emptyenv())
 
-  # If it's not already cached, add it.
-  if (is.null(srcref_cache[[filename]])) {
-    # Name of package containing filename.
-    pkgdir <- sub(".*/([^/]+)/R/[^/]+", "\\1", filename)
+  lapply(pkgs, function(pkg) {
+    srcrefs <- get_pkg_srcrefs(pkg)
+    if (length(srcrefs) > 0)
+      list2env(srcrefs, srcfile_cache)
+  })
 
-    # Try to infer the package name from the package directory.
-    pkg <- infer_package_name(pkgdir)
-    if (is.null(pkg)) {
-      return(NULL)
-    }
-    load_pkg_into_cache(pkg, srcref_cache)
-  }
-
-  srcref_cache[[filename]]
+  srcfile_cache
 }
 
 
-load_pkg_into_cache <- function(pkg, srcref_cache) {
-  # Now grab an arbitrary function from that package
-  ns <- asNamespace(pkg)
+# Given a namespace, try to extract source code. It does this by looking at
+# functions in the namespace and getting the appropriate attributes. This
+# returns a named list with all sources for a package.
+get_pkg_srcrefs <- function(pkg) {
+  ns_env <- asNamespace(pkg)
 
-  # Given a namespace, try to extract source code. It does this by looking at
-  # arbitrary functions in the namespace and getting the appropriate attributes.
-  # This returns all sources for a package in a single character vector.
-  extract_source_from_namespace <- function(ns) {
-    ns <- as.list(ns)
-
-    # Look at all the objects in the namespace; when one is found with the
-    # appropriate source ref info, return the text.
-    for (i in seq_along(ns)) {
-      x <- ns[[i]]
-      if (is.function(x) && !is.null(attr(x, "srcref", exact = TRUE))) {
-        srcref <- attr(x, "srcref", exact = TRUE)
-        srcfile <- attr(srcref, "srcfile", exact = TRUE)
-        original <- srcfile$original
-        return(original$lines)
-      }
-    }
-
-    NULL
-  }
-
-  src <- extract_source_from_namespace(ns)
-
-  # Failed at extracting source refs
-  if (is.null(src)) {
-    return(NULL)
-  }
-
-  # Drop first line (simply contains package name)
-  src <- src[-1]
-
-  # Split out all files into separate entries in a list
+  # Given a char vector with contents of an entire package, split out all
+  # files into separate entries in a list.
   full_src_to_file_contents <- function(src) {
+    # Drop first line (simply contains package name)
+    src <- src[-1]
+
     # Lines which contain filenames. Have a format like:
     #   "#line 1 \"/tmp/Rtmp6W0MLC/R.INSTALL1a531f3beb59/ggplot2/R/aaa-.r\""
     filename_idx <- grep('^#line 1 "', src)
@@ -112,65 +59,79 @@ load_pkg_into_cache <- function(pkg, srcref_cache) {
     file_contents
   }
 
-  file_contents <- full_src_to_file_contents(src)
-  list2env(file_contents, envir = srcref_cache)
+  # Get all objects in package. Need to filter out S4 mangled names (.__T__)
+  ns_names <- grep("^\\.__[TC]__", ls(ns_env, all.names = TRUE), value = TRUE,
+      invert = TRUE, fixed = FALSE)
+
+  files <- list()
+
+  for (name in ns_names) {
+    x <- ns_env[[name]]
+    if (is.function(x)) {
+      srcref <- getSrcref(x)
+
+      # If any function lacks source refs, then no functions in the package will
+      # have them. Quit early to save time.
+      if (is.null(srcref))
+        break
+
+      # There are two possible formats for source refs. If the file was
+      # loaded with `source()` (as with `devtools::load_all()`), the lines
+      # will just be the cotents of that one file. If the file was from a
+      # package that was built and installed the normal way, it will contain
+      # the all sources for the entire package.
+      srcfile <- attr(srcref, "srcfile", exact = TRUE)
+
+      if (!is.null(srcfile$lines)) {
+        # Was loaded with `source(). If we don't already have the source for
+        # this file, save them and keep going.
+        if (is.null(files[[srcfile$filename]])) {
+          files[[srcfile$filename]] <- paste(srcfile$lines, collapse = "\n")
+        }
+
+      } else if (!is.null(srcfile$original$lines)) {
+        # Was from a built package and therefore contains source for all
+        # files in the package. We can extract source code for all files and
+        # return.
+        files <- full_src_to_file_contents(srcfile$original$lines)
+        break
+
+      } else {
+        # Shouldn't get here -- if so, this is an unexpected configuration.
+        stop("Unexpected format for source refs.")
+      }
+    }
+  }
+
+  files
 }
 
 
-# Given a string like "webshot" or "wch-webshot-d5979a2", try to infer the name
-# of the package. `devtools::install_github()` will result in names like
-# "wch-webshot-d5979a2", but downloading a zip file from GitHub's web interface
-# will result in "webshot-master" or "webshot-feature-branch". So the possible
-# formats include: "xxx-pkgname-yyy", "pkgname-xxx-yyy", and "pkgname-xxx", and
-# possibly others. After guessing what the name is, this function checks that a
-# package by that name is installed. If so, it will return it. If this function
-# can't infer the name (a package must be installed with the inferred name), it
-# returns NULL.
-infer_package_name <- function(str) {
-  parts <- strsplit(str, "-", fixed = TRUE)[[1]]
+# Looks at all loaded namespaces, and returns a data frame that has mappings
+# between filenames and the package they belong to.
+package_map <- function(loaded_ns = loadedNamespaces()) {
 
-  # str was something like "webshot".
-  if (length(parts) == 1) {
-    if (is_installed(str)) {
-      return(str)
-    } else {
-      message("Unable to infer package name from directory named '", str, "'")
-      return(NULL)
+  # Helper to extract captures
+  regcaptures <- function(x, m) {
+    ind <- !is.na(m) & m > -1L
+    so <- attr(m, "capture.start")[ind]
+    eo <- so + attr(m, "capture.length")[ind] - 1L
+    substring(x[ind], so, eo)
+  }
+
+  res <- lapply(loaded_ns, function(ns) {
+    src_lines <- extract_source_from_namespace(ns)
+
+    if (!is.null(src_lines)) {
+      # Find and extract the line directives
+      m <- regexpr("^#line \\d+ \"(.*)\"$", src_lines, perl = TRUE)
+      regcaptures(src_lines, m)
     }
-  }
+  })
 
-  # If there's a single "-", the format is probably "pkgname-xxx", though we'll
-  # try "xxx-pkgname" also.
-  if (length(parts) == 2) {
-    if (is_installed(parts[1])) {
-      return(parts[1])
-    } else if (is_installed(parts[2])) {
-      return(parts[2])
-    } else {
-      message("Unable to infer package name from directory named '", str, "'")
-      return(NULL)
-    }
-  }
-
-  # If there are two "-", then the format could be "xxx-pkgname-yyy" or
-  # "pkgname-xxx-yyy". The first is most likely (because devtools uses it), so
-  # we'll try it first.
-  if (length(parts) == 3) {
-    if (is_installed(parts[2]))
-      return(parts[2])
-
-    if (is_installed(parts[1]))
-      return(parts[1])
-  }
-
-  # None of the above matched, so it could be "xxx-yyy-pkgname", or there could
-  # be more parts. We'll just try each piece.
-  for (i in seq(3, length(parts))) {
-    if (is_installed(parts[i]))
-      return(parts[i])
-  }
-
-  # If we got here, nothing worked.
-  message("Unable to infer package name from directory named '", str, "'")
-  NULL
+  data.frame(
+    filename = unlist(res),
+    package = rep(loaded_ns, times = lengths(res)),
+    stringsAsFactors = FALSE
+  )
 }
