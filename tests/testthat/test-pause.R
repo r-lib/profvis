dbac3a9e43777fafb873ca64022ef5768aea5793
Hadley Wickham <h.wickham@gmail.com>
2024-09-09 10:30:35 -0500

    Add a little more wiggle room

--- tests/testthat/test-pause.R
+++ tests/testthat/test-pause.R
@@ -1,6 +1,7 @@
 test_that("pause takes expected time", {
-  time <- system.time(pause(0.21))[[3]]
-  expect_gte(time, 0.2)
+  time <- system.time(pause(0.2))[[3]]
+  # system.time is a little inaccurate so allow 10% padding
+  expect_true(abs(time - 0.2) < 1e-2)
 })
 
 test_that("works with integers", {
