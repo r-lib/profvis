9061761ea51c1cef0978f5cc48df0caf200b41c0
Hadley Wickham <h.wickham@gmail.com>
2024-09-12 16:04:50 -0500

    Improved helpers

--- tests/testthat/test-profvis.R
+++ tests/testthat/test-profvis.R
@@ -1,26 +1,26 @@
 test_that("Irrelevant stack is trimmed from profiles (#123)", {
   skip_on_cran()
   skip_on_covr()
-  
+
   f <- function() pause(TEST_PAUSE_TIME)
 
   out <- repro_profvis(f(), simplify = FALSE)
-  expect_equal(profvis_modal_value(out$x$message$prof), "pause f")
+  expect_equal(profile_mode(out), "pause f")
 
   out <- profvis(f(), simplify = TRUE, rerun = "pause", interval = 0.005)
-  expect_equal(profvis_modal_value(out$x$message$prof), "pause f")
+  expect_equal(profile_mode(out), "pause f")
 
   out <- repro_profvis(f(), simplify = TRUE)
-  expect_equal(profvis_modal_value(out$x$message$prof), "pause f")
+  expect_equal(profile_mode(out), "pause f")
 })
 
 test_that("defaults to elapsed timing", {
   skip_on_cran()
   skip_on_covr()
   skip_if_not(has_event())
-  
+
   f <- function() Sys.sleep(TEST_PAUSE_TIME)
 
   out <- repro_profvis(f(), rerun = "Sys.sleep")
-  expect_equal(profvis_modal_value(out$x$message$prof), "Sys.sleep f")
+  expect_equal(profile_mode(out), "Sys.sleep f")
 })
